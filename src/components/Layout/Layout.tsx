a6f8dbd112ad24d71cf706d4f1a100bef6854517
idan_sagi <idansagi@gmail.com>
2025-08-12 14:59:33 +0000

    spinner

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,13 @@ import type { PropsWithChildren } from 'react'
 import React, { Suspense } from 'react'
 
 import Navbar from '../Navbar'
+import { Spinner } from '../ui/loader'
 
 import styles from './Layout.module.css'
 
 const loader = () => (
   <div className="h-full min-h-full flex items-center justify-center text-black">
-    Loading...
+    <Spinner>Loading...</Spinner>
   </div>
 )
 
